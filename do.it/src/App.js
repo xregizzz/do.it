61f87719004ef560e6794a75e6af7254ff65be83
Regis T. Silveira <xregizzz@gmail.com>
2022-09-26 16:04:29 -0300

    feat: signup api integrations and toastify returns

--- do.it/src/App.js
+++ do.it/src/App.js
@@ -1,10 +1,23 @@
 import RoutesApp from "./routes";
 import GlobalStyle from "./styles/global";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <RoutesApp />
     </>
   );
